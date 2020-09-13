03a456c302d6207de79b7834ae05ae250c5bd2e3
Hsuan-Hau Liu <hsuanhauliu@gmail.com>
2020-09-13 13:23:45 -0700

    fix: incorrect syntax; color changing not working

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 let changeColor = document.getElementById('changeColor');
 
 changeColor.onclick = function(element) {
-  let color = element.target.value;
+  let color = 'green';
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
       tabs[0].id,
-      {code: 'document.body.style.backgroundColor = "' + color + '";'});
+      {code: 'document.body.style.backgroundColor="' + color + '";'});
   });
 };
